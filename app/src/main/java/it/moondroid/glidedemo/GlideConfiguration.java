07e5af38d52e2cfbbd861f39f52fa4fd00cf7041
Marco Granatiero <moondroidapps@gmail.com>
2015-05-20 20:40:07 +0200

    Initial commit

---
+++ app/src/main/java/it/moondroid/glidedemo/GlideConfiguration.java
@@ -0,0 +1,26 @@
+package it.moondroid.glidedemo;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.module.GlideModule;
+
+/**
+ * Created by Marco on 20/05/2015.
+ */
+public class GlideConfiguration implements GlideModule {
+
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        // Apply options to the builder here.
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+        // register ModelLoaders here.
+    }
+}
---
+++ app/src/main/java/it/moondroid/glidedemo/MainActivity.java
@@ -0,0 +1,51 @@
+package it.moondroid.glidedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        Glide.with(this)
+                .load("http://inthecheesefactory.com/uploads/source/glidepicasso/cover.jpg")
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .bitmapTransform(new CropCircleTransformation(this))
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.imagenotfound)
+                .into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
